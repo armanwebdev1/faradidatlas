c4c08856f66c7a36947bc447cb8190618c55f485
armanwebdev1 <ahmadpour.web@gmail.com>
2026-06-27 14:06:16 +0330

    fixed mobile numbers

--- lib/contact-info.ts
+++ lib/contact-info.ts
@@ -3,12 +3,12 @@ export const publicContactEmail = "info.faradidco@gmail.com";
 export const publicPhoneNumbers = [
   {
     value: "09128340552",
-    display: "0912 834 0552",
-    href: "tel:+989128340552",
+    display: "0912 834 0551",
+    href: "tel:+989128340551",
   },
   {
     value: "09128340551",
-    display: "0912 834 0551",
-    href: "tel:+989128340551",
+    display: "0912 834 0552",
+    href: "tel:+989128340552",
   },
 ] as const;
