e2820baf6cb9f22843c4bbb76f16720ccae51a7d
armanwebdev1 <ahmadpour.web@gmail.com>
2026-07-10 18:40:54 +0330

    reduced max pool to 2

--- payload.config.ts
+++ payload.config.ts
@@ -66,7 +66,7 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URL || "",
-      max: 5,
+      max: 2,
       idleTimeoutMillis: 10000,
     },
   }),
