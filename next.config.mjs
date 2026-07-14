3077a259003bb00daa25fefb8a8d0946052e6ebc
armanwebdev1 <ahmadpour.web@gmail.com>
2026-07-14 12:23:05 +0330

    Reapply "final optimization"
    This reverts commit e37b013009f41f148b153906e94381bb59786dee.

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,9 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -90,6 +93,15 @@ const nextConfig = {
       },
       {
         source: "/fonts/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/brand/:path*",
         headers: [
           {
             key: "Cache-Control",
