595b02c61185da12c886b3840ddf25b4c24cba3c
armanwebdev1 <ahmadpour.web@gmail.com>
2026-08-06 10:30:47 +0330

    restoration

--- components/scroll-restoration.tsx
+++ components/scroll-restoration.tsx
@@ -7,7 +7,7 @@ export function ScrollRestoration() {
   const pathname = usePathname()
 
   useEffect(() => {
-    window.scrollTo(0, 0)
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" })
   }, [pathname])
 
   return null
